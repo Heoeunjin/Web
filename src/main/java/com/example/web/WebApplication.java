c8465f394a41f9f610898db8dc70c24c4e9df42e
ghdtldus <ghdtldus03a@gmail.com>
2024-02-21 13:19:25 +0900

    pracitce

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -1,6 +1,7 @@
 package com.example.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplicationAotProcessor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +10,5 @@ public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-
+    SpringApplicationAotProcessor dfd;
 }
